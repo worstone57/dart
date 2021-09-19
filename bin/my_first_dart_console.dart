f56e6af29168e4215fd079abc8d6c144c1dfb21b
worstone57 <worstone57@gmail.com>
2021-09-19 20:56:21 +0700

    file dasar untuk belajar

--- bin/my_first_dart_console.dart
+++ bin/my_first_dart_console.dart
@@ -1,5 +1,3 @@
-import 'package:my_first_dart_console/my_first_dart_console.dart' as my_first_dart_console;
-
 void main(List<String> arguments) {
-  print('Hello world: ${my_first_dart_console.calculate()}!');
+  print('Hellow world!');
 }
--- test/my_first_dart_console_test.dart
+++ test/my_first_dart_console_test.dart
@@ -1,8 +1,7 @@
-import 'package:my_first_dart_console/my_first_dart_console.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    // expect(calculate(), 42);
   });
 }
