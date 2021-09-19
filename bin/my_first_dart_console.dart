3e3ea8e3f3c3ff1c87cc30520b8b27c0725ebfd6
worstone57 <worstone57@gmail.com>
2021-09-19 20:48:07 +0700

    file dasar

---
+++ bin/my_first_dart_console.dart
@@ -0,0 +1,5 @@
+import 'package:my_first_dart_console/my_first_dart_console.dart' as my_first_dart_console;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_first_dart_console.calculate()}!');
+}
