3af364bd13278baaa41245c3b4d83044cc322c79
worstone57 <worstone57@gmail.com>
2021-09-20 11:48:31 +0700

    pbo dart 02. switch

--- bin/my_first_dart_console.dart
+++ bin/my_first_dart_console.dart
@@ -2,34 +2,32 @@ import 'dart:io'; //digunakan untuk meminta input dari console
 
 void main(List<String> arguments) {
   int number = int.tryParse(stdin.readLineSync());
-  String output;
 
-  //if else cara biasa jika ada 3 kondisi
-  if (number > 0) {
-    print("positif");
-  } else if (number < 0) {
-    print("negatif");
-  } else {
+  //dibawah ini adalah bentuk if elsenya
+  if (number == 0) {
     print("nol");
-  }
-
-  //if else cara biasa jika ada 2 kondisi
-  if (number > 0) {
-    output = "positif";
+  } else if (number == 1) {
+    print("satu");
+  } else if (number == 2) {
+    print("dua");
   } else {
-    output = "negatif atau nol";
+    print("bilangan lain");
   }
-  print(output);
 
-  //if else cara singkat jika ada dua kondisi
-
-  output = (number > 0) ? "positif" : "negatif atau nol";
-  print(output);
-
-  //if else cara singkat jika ada dua kondisi langsung dicetak
-  print((number > 0) ? "positif" : "negatif atau nol");
-
-  //if else cara singkat jika ada dua kondisi langsung dicetak
-  //isi nya yang "positif" : "negatif atau nol" tidak harus string
-  print((number > 0) ? number : number * -1);
+  //dibawah ini adalah bentuk switchnya
+  int number2 = int.tryParse(stdin.readLineSync());
+  switch (number2) {
+    case 0:
+      print("nol");
+      break;
+    case 1:
+      print(number2);
+      print("satu");
+      break;
+    case 2:
+      print("dua");
+      break;
+    default:
+      print("bilangan lain");
+  }
 }
